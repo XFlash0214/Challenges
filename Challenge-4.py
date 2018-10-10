5698ecaafd05f2801acb5e8ef14a415f5134e4ca
Sampson Neyman <43590651+XFlash0214@users.noreply.github.com>
2018-10-09 19:33:15 -0500

    Add files via upload

---
+++ Challenge-4.py
@@ -0,0 +1,17 @@
+def last_two(s):
+    s1=s[-2:]
+    output=s1*3
+    return output
+if last_two("hello")==("lololo"):
+    print("Correct")
+else:
+    print("Incorrect")
+    
+def last_two(s):
+    s1=s[-2:]
+    output=s1*3
+    return output
+if last_two("hi")==("hihihi"):
+    print("Correct")
+else:
+    print("Incorrect")
